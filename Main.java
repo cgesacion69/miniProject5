ede2d95a89c3d9d44b184f16a1b6e6594481b563
cgesacion <cameron.gesacion@patelhs.org>
2021-02-19 17:40:29 +0000

    Completed main method

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
+  static int myMethod(int x) {
+    return 5 + x;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner input = new Scanner(System.in);
+    System.out.println(myMethod(3));
+
+    int result = sum(10);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+    }
   }
 }
